be2f0194ae6b34849584ca47fd0045881d517f83
elzino <ljh0728@snu.ac.kr>
2019-06-06 11:32:10 +0900

    changed optimizer from RMSprop to Adam

--- train.py
+++ train.py
@@ -18,18 +18,18 @@ from vecenv.shmem_vec_env import ShmemVecEnv
 if __name__ == '__main__':
     #args
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    n_env = 2
-    n_step = 5
+    n_env = 12
+    n_step = 20
     gamma = 0.99
     ent_coef = 0.01
     vf_coef = 0.5
     max_grad_norm = 0.5
-    save_model_per_epoch = 10
-    num_updates = 100
+    save_model_per_epoch = 1000
+    num_updates = 200000
 
-    lr = 0.001
-    alpha = 0.99
-    epsilon = 1e-05
+    lr = 0.0005
+#    alpha = 0.99
+#    epsilon = 1e-05
 
     env_id = 'Breakout-v0'
     envs = [make_env(env_id) for _ in range(n_env)]
@@ -49,7 +49,8 @@ if __name__ == '__main__':
 
     runner = Runner(envs, step_policy, n_step, gamma)
 
-    optimizer = optim.RMSprop(train_policy.parameters(), lr=lr, alpha=alpha, eps=epsilon)
+    optimizer = optim.Adam(train_policy.parameters(), lr=lr)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, int(num_updates/10), gamma=0.5)
 
     for i in tqdm(range(num_updates)):
         mb_obs, mb_rewards, mb_values, mb_actions = runner.run()
@@ -76,7 +77,7 @@ if __name__ == '__main__':
 #            if param.grad is not None:
 #                param.grad.data.clamp_(-max_grad_norm, max_grad_norm)
 
-        optimizer.step()
+        scheduler.step()
         step_policy.load_state_dict(train_policy.state_dict())
 
         if i % save_model_per_epoch == 0 or i == (num_updates - 1):
